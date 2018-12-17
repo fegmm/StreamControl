9a292a4d27efd83ccc24d0abb138a4ed77485990
Samuel Weber <samuel@familie-weber.de>
2018-12-17 17:34:21 +0100

    MainWindow is now not focusable
    Except for alt tabbing into it, when using the mouse inside of the application the focus will not be removed from other programms

--- StreamControl/Views/MainWindow.xaml.cs
+++ StreamControl/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace StreamControl.Views
 {
@@ -12,5 +15,23 @@ namespace StreamControl.Views
             InitializeComponent();
             this.MinHeight = this.MaxHeight = SystemParameters.WorkArea.Height;
         }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SetWindowLong(helper.Handle, GWL_EXSTYLE,
+                GetWindowLong(helper.Handle, GWL_EXSTYLE) | WS_EX_NOACTIVATE);
+        }
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
     }
 }
